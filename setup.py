8fd4fe53b3c93ce474c8516590f65cf704b69e02
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2022-12-22 15:36:31 +0100

    Allow local pip installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ts_spatial_filter',
+      version='1.1.0',
+      description='Time series filter',
+      url='https://github.com/Charles-Fieseler-Vienna/ts_spatial_filter',
+      author='statefb',
+      author_email='',
+      license='MIT',
+      packages=setuptools.find_namespace_packages(),
+      zip_safe=False)
